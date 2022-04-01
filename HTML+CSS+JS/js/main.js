9f890d54c2b9f08c6ba64c83ad6d4e25220ac264
Gabriel Fernandes Ramos <gabrielframos02@gmail.com>
2022-04-01 19:58:25 -0400

    Correcao Preloader

--- HTML+CSS+JS/js/main.js
+++ HTML+CSS+JS/js/main.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-var btnContact = document.querySelector('.jl-btn-contact');
-
-=======
-<<<<<<< Updated upstream
-=======
 //Declarando Variáveis
 var btnContact = document.querySelector('.jl-btn-contact');
 
@@ -18,15 +12,9 @@ window.addEventListener('load', function() {
 }) 
 
 //Abrindo e Fechando Informações de Contato
->>>>>>> master
 btnContact.addEventListener('click', function(){
     var boxContact = document.querySelector('.jl-contact-info');
     boxContact.classList.toggle('jl-is-open');
     
     this.classList.toggle('jl-change-icon');
 });
-
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
->>>>>>> master
