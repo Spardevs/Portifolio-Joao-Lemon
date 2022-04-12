a43b9d46cd571f52574dac6516a3f411c9960c02
Gabriel Fernandes Ramos <gabrielframos02@gmail.com>
2022-04-12 19:37:17 -0400

    Criacao modal orcamento

--- HTML+CSS+JS/js/main.js
+++ HTML+CSS+JS/js/main.js
@@ -1,6 +1,6 @@
 //Declarando Variáveis
 var btnContact = document.querySelector('.jl-btn-contact');
-
+var toggleModal = document.querySelectorAll('.jl-toggle-modal')
 //Page Preloader
 window.addEventListener('load', function() {
     var pagePreLoader = document.querySelector('.jl-preloader');
@@ -18,3 +18,16 @@ btnContact.addEventListener('click', function(){
     
     this.classList.toggle('jl-change-icon');
 });
+
+// Abrindo e fechando o Modal de orçamento
+
+for(var i = 0; i < toggleModal.length; i++){
+    toggleModal[i].addEventListener('click', function(){
+        var overlay = document.querySelector('.jl-overlay');
+        var modalOrcamento = document.querySelector('#jl-modal-orcamento');
+
+        overlay.classList.toggle('jl-is-open');
+        modalOrcamento.classList.toggle('jl-is-open');
+        modalOrcamento.classList.toggle('jl-slide-top-in');
+    })
+}
