069b49840aa10cbbae1ae9162e7c451615dbd5d5
Gabriel Fernandes Ramos <gabrielframos02@gmail.com>
2022-04-29 16:39:50 -0400

    Finalização da página home

--- HTML+CSS+JS/js/slider.js
+++ HTML+CSS+JS/js/slider.js
@@ -12,8 +12,9 @@ var sliderPos = 0;
 var currentSlide = document.querySelector('.jl-current-slide');
 var totalSlide = document.querySelector('.jl-total-slide');
 var currentCounter = 1;
-var navItems = document.querySelectorAll('.jl-item-navigator a')
+var navItems = document.querySelectorAll('.jl-item-navigator a');
 var navCounter = document.querySelector('.jl-navigator-counter span');
+
 
 //Capturando larguras individuais
 var containerWidth = sliderContainer.parentElement.offsetWidth;
@@ -33,6 +34,7 @@ sliderList.style.width = sliderListWidth + 'px';
 
 //Fazendo Animaçao do Slider onClick
 
+
 //HANDLERS
 
 //Next Slide Animaçao
@@ -46,7 +48,8 @@ var nextSlideAnim = function () {
     sliderPos -= containerWidth;
     anime({
         targets: sliderList,
-        translateX: sliderPos
+        translateX: sliderPos,
+        easing: 'cubicBezier(0,1.01,.32,1)'
     });
 }
 
@@ -59,47 +62,45 @@ var prevSlideAnim = function () {
     sliderPos += containerWidth;
     anime({
         targets: sliderList,
-        translateX: sliderPos
+        translateX: sliderPos,
+        easing: 'cubicBezier(0,1.01,.32,1)'
     });
 }
 
-// Counter Formatter
-var counterFormatter = function(n){
-    if(n < 10){
-        return '0'+n;
-    }else{
-        returnn;
+//Counter Formater
+var counterFormatter = function (n) {
+    if (n < 10) {
+        return '0' + n;
+    } else {
+        return n;
     }
 }
 
-// Counter Add
-
-var counterAdd = function(){
-    if((currentCounter >=0 ) && (currentCounter < sliderTotalItems)){
+//Counter Add
+var counterAdd = function () {
+    if ((currentCounter >= 0) && (currentCounter < sliderTotalItems)) {
         currentCounter++;
         currentSlide.innerHTML = counterFormatter(currentCounter);
         navCounter.innerHTML = counterFormatter(currentCounter);
     }
 }
 
-// Counter Remove
-
-var counterRemove = function(){
-    if((currentCounter > 1 ) && (currentCounter <= sliderTotalItems)){
+//Counter Remove
+var counterRemove = function () {
+    if ((currentCounter > 1) && (currentCounter <= sliderTotalItems)) {
         currentCounter--;
         currentSlide.innerHTML = counterFormatter(currentCounter);
         navCounter.innerHTML = counterFormatter(currentCounter);
     }
 }
 
-
-// Set Active Nav
-
-var setActiveNav = function(){
-    for(var nv = 0; nv < navItems.length; nv++){
+//Set Active Nav
+var setActiveNav = function () {
+    for (var nv = 0; nv < navItems.length; nv++) {
         let myNavNum = parseInt(navItems[nv].getAttribute('data-nav'));
-        if(myNavNum === currentCounter){
-            navItems[nv].classList.add('jl-item-active')
+
+        if (myNavNum === currentCounter) {
+            navItems[nv].classList.add('jl-item-active');
 
             anime({
                 targets: '.jl-item-active',
@@ -109,8 +110,22 @@ var setActiveNav = function(){
     }
 }
 
-var changeActive = function(){
-    for(var rm = 0; rm < navItems.length; rm++){
+//Set Active Slide
+var setActiveSlide = function () {
+    for (var sld = 0; sld < sliderItem.length; sld++) {
+        let mySlideNum = parseInt(sliderItem[sld].getAttribute('data-slide'));
+
+        if (mySlideNum === currentCounter) {
+            sliderItem[sld].classList.add('jl-slide-active');
+            sliderItem[sld].querySelector('.jl-portfolio-item-box').classList.add('jl-scale-right');
+            sliderItem[sld].querySelector('.jl-portfolio-item-thumb img').classList.add('jl-scale-up');
+            sliderItem[sld].querySelector('.jl-portfolio-item-info').classList.add('jl-fade-from-left');
+        }
+    }
+}
+
+var changeActive = function () {
+    for (var rm = 0; rm < navItems.length; rm++) {
         navItems[rm].classList.remove('jl-item-active');
         anime({
             targets: navItems[rm],
@@ -118,17 +133,26 @@ var changeActive = function(){
         });
     }
 
+    for (var rms = 0; rms < sliderItem.length; rms++) {
+        sliderItem[rms].classList.remove('jl-slide-active');
+        sliderItem[rms].querySelector('.jl-portfolio-item-box').classList.remove('jl-scale-right');
+        sliderItem[rms].querySelector('.jl-portfolio-item-thumb img').classList.remove('jl-scale-up');
+        sliderItem[rms].querySelector('.jl-portfolio-item-info').classList.remove('jl-fade-from-left');
+    }
     setActiveNav();
+    setActiveSlide();
 }
 
-// Actions
 
+
+//ACTIONS
 totalSlide.innerHTML = counterFormatter(sliderTotalItems);
 
 anime({
     targets: '.jl-item-active',
     width: 90
 });
+
 
 nextItem.addEventListener('click', function () {
     nextSlideAnim();
